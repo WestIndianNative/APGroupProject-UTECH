46f06d3a8d0ad484237c82b6344adf0d52b5313d
WestIndianNative <102269758+WestIndianNative@users.noreply.github.com>
2022-07-14 22:17:40 -0500

    Andre

--- src/view/Services.java
+++ src/view/Services.java
@@ -1,4 +1,4 @@
-package crmGUI;
+package view;
 
 import java.awt.Color;
 import java.awt.Font;
--- src/view/ViewServiceRequest.java
+++ src/view/ViewServiceRequest.java
@@ -1,4 +1,4 @@
-package crmGUI;
+package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
